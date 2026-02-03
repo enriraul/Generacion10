fa40ebb72935d151d25f7558cdde1012eef746ff
Enrique Raúl OR <128866748+enriraul@users.noreply.github.com>
2026-02-03 10:23:30 -0600

    Add files via upload

---
+++ Módulo4/Sesión1/Main.java
@@ -0,0 +1,56 @@
+public class Main{
+    public static void main(String[] args) {
+        /*
+            Comandos básicos de bash
+            cd - change Directory - Cambiar de carpeta
+            cd .. -Regresar una raíz del archivo
+            ls - list - Mostrar los elementos dentro de la dirección que nos encontramos
+            pwd - path with directory- Mostrar la ruta donde estamos posicionados
+            mkdir make directory - Crear una carpeta
+            clear limpiar ventana
+
+        */
+
+
+        //
+        /* 
+            Tipo de datos y Operadores
+        */
+        
+        int edad=33;  
+        float estatura= 13.5f;
+        double estadoCuenta = 15.2;
+        boolean MayorEdad = true; //true false
+        char Sexo='M';
+
+        //Operadores
+        /*
+            Operadores Matemáticos
+            
+                Suma +
+                Resta -
+                División  /
+                Multiplicación *
+                módulo %
+
+            Operadores Comparativos/Relativos
+                < Menor que
+                > Mayor que
+                == Igual
+                != Diferente/Desigual
+                <= Menor o igual qué
+                >= Mayor o igual qué
+
+            Operadores Lógicos
+
+                and Conjunción
+                or Disyunción
+                not Negación
+
+        */
+        // Impresión en Java
+
+        System.out.printf("Hola Mundo");
+
+    }
+}
