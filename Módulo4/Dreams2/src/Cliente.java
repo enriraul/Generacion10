627d8c7e6ee664b47509321892ef8b888ec6cae7
Enrique Raúl OR <128866748+enriraul@users.noreply.github.com>
2026-03-05 08:52:36 -0600

    Add files via upload

---
+++ Módulo4/Dreams2/src/Cliente.java
@@ -0,0 +1,36 @@
+public class Cliente {
+
+    private String nombre;
+    private String correo;
+
+    // Constructor vacío
+    public Cliente() {
+    }
+
+    // Constructor con parámetros
+    public Cliente(String nombre, String correo) {
+        this.nombre = nombre;
+        this.correo = correo;
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return this.nombre;
+    }
+    public String getCorreo() {
+        return this.correo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+ 
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    // Método info()
+    public String info() {
+        return "Cliente: " + this.nombre + " | Correo: " + this.correo;
+    }
+}
