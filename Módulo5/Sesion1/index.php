900abc4a46b591d883f58e7fb24686b86ad20d87
Enrique Raúl OR <128866748+enriraul@users.noreply.github.com>
2026-03-25 09:19:42 -0600

    Add files via upload

---
+++ Módulo5/Sesion1/index.php
@@ -0,0 +1,36 @@
+<?php
+/*
+    forma de comentar dentro de php
+
+    Sintaxis
+*/
+?>
+
+
+<?
+    echo "Hola mundo";/*Imprimir varios elementos*/
+    print "Hola mundo";/*Imprimir un solo elemento*/
+?>
+<?
+    /*
+        Comentarios
+        Segmentos de texto grandes    
+    */
+    //Comentario por linea
+    #Comentario por linea
+?>
+
+<?
+    //Declaracion de variables y tipos de datos.
+    $nombre;
+    $_nombre;
+    $Nombre;
+    $dato = 10;
+    $dato = "diez";
+
+
+    $numero1 =1;
+    $numero2 = "3";
+    $numero3 = $numero1 + $numero2;
+    $numero4 = $numero1 . $numero2;
+?>
---
+++ Módulo5/Sesion2/index.php
@@ -0,0 +1,72 @@
+<?
+    //operadores
+
+    $a = 10;
+    $b = 5;
+
+    //Operadores aritmeticos
+    $suma = $a + $b;
+    $resta = $a - $b;
+    $multiplicacion = $a * $b;
+    $division = $a / $b;
+    $residuo = $a % $b;
+
+    //Operadores comparativos/relacionales
+    $a == $b 
+    $a === $b  //Estrictamente igual 
+    $a != $b
+    $a > $b
+    $a < $b
+
+    //Operadores logicos
+    $a && $b //and
+    $a || $b //or
+    !$a //negacion
+
+?>
+
+
+<?
+    //Estructura de control
+    #condicional
+    if($a){
+    }
+    else if ($a){
+    }
+    else{
+    }
+    #switch
+    switch($a){
+        case opcion:
+            break;
+        case opcion2:
+            break;
+        case opcion3:
+            break;
+    }
+    #Ciclos for
+    for($i=1; $i<=5;$i++){
+
+    }
+
+    #Ciclos while
+    while($a>=5){
+
+    }
+    
+    #Ciclos do-while
+    do{
+
+    }while(condicional);
+
+?>
+<?
+    #foreach
+    $arreglo = ["ana","pedro","luis"];
+    foreach($nombre as $arreglo){
+
+    }
+    foreach($arreglo as $indice=> $valor){
+        echo $indice, $valor;
+    }
+?>
